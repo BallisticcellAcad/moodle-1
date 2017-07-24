a72e4d3942c6b067308d5cb554b72a290bc18afa
Svetoslav Georgiev <svetoslav.georgiev@84bits.io>
2017-07-24 15:03:55 +0300

    AS-94

--- blocks/course_overview_gl/lang/bg/block_course_overview_gl.php
+++ blocks/course_overview_gl/lang/bg/block_course_overview_gl.php
@@ -3,8 +3,8 @@ require_once($CFG->dirroot.'/blocks/course_overview_gl/config.php');
 
 $categoryName = GL_CATEGORY_NAME;
 
-$string['course_overview_gl:addinstance'] = "Добавен е нов блок Преглед на $categoryName курсовете";
-$string['course_overview_gl:myaddinstance'] = "Добавен е нов блок Преглед на $categoryName курсовете на Моето табло";
-$string['defaultmaxcoursesdesc'] = "Максимален брой курсове, които да се показват в блок Преглед на $categoryName kursowete, 0 значи показване на всички курсове от $categoryName";
-$string['pluginnamecustom'] = "Преглед на $categoryName курсовете";
-$string['pluginname'] = "Преглед на $categoryName курсовете";
+$string['course_overview_gl:addinstance'] = "Добавен е нов блок Бизнес обучения";
+$string['course_overview_gl:myaddinstance'] = "Добавен е нов блок Бизнес обучения на Моето табло";
+$string['defaultmaxcoursesdesc'] = "Максимален брой курсове, които да се показват в блок Бизнес обучения, 0 значи показване на всички курсове от Бизнес обучения";
+$string['pluginnamecustom'] = "Бизнес обучения";
+$string['pluginname'] = "Бизнес обучения";
--- blocks/course_overview_rs/lang/bg/block_course_overview_rs.php
+++ blocks/course_overview_rs/lang/bg/block_course_overview_rs.php
@@ -1,10 +1,8 @@
 <?php
 require_once($CFG->dirroot.'/blocks/course_overview_rs/config.php');
 
-$categoryName = RS_CATEGORY_NAME;
-
-$string['course_overview_rs:addinstance'] = "Добавен е нов блок Преглед на $categoryName курсовете";
-$string['course_overview_rs:myaddinstance'] = "Добавен е нов блок Преглед на $categoryName курсовете на Моето табло";
-$string['defaultmaxcoursesdesc'] = "Максимален брой курсове, които да се показват в блок Преглед на $categoryName kursowete, 0 значи показване на всички курсове от $categoryName";
-$string['pluginnamecustom'] = "Преглед на $categoryName курсовете";
-$string['pluginname'] = "Преглед на $categoryName курсовете";
+$string['course_overview_rs:addinstance'] = "Добавен е нов блок Езикови курсове";
+$string['course_overview_rs:myaddinstance'] = "Добавен е нов блок Езикови курсове на Моето табло";
+$string['defaultmaxcoursesdesc'] = "Максимален брой курсове, които да се показват в блок Езикови курсове, 0 значи показване на всички курсове от Езикови курсове";
+$string['pluginnamecustom'] = "Езикови курсове";
+$string['pluginname'] = "Езикови курсове";
